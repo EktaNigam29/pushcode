132faafe5b95fc617e453b845d4c2e2c51b9e7d4
Ekta <nigam.ekta29@gmail.com>
2025-03-28 19:07:32 +0530

    Java code

---
+++ Practise_Questions.java
@@ -0,0 +1,104 @@
+package basics.programm;
+
+import java.util.Arrays;
+
+public class Practise_Questions {
+	
+
+
+public static void main(String[] args) {
+
+/*String name="ektaabhi";
+System.out.println(name.toUpperCase());
+System.out.println(name.toLowerCase());
+System.out.println(name.concat("nigam"));
+System.out.println(name.length());
+System.out.println(name.substring(2));
+System.out.println(name.substring(2, 5));
+System.out.println(name.replaceAll("[a-z]", ""));
+System.out.println(name.matches("(.*)a(.*)"));
+System.out.println(name.endsWith("h"));
+System.out.println(name.charAt(0));
+System.out.println(name.indexOf("k"));
+System.out.println(name.isEmpty());
+System.out.println(name.equalsIgnoreCase("Ekta"));
+System.out.println(name.trim());
+
+String name[] = new String[2];
+name[0]="ekta";
+name[1]="abhi";
+name[2]="abhik";
+for(int i=0;i<=name.length;i++)
+	
+{
+	System.out.println(name[i]);
+}
+System.out.println(Arrays.toString(name));*/
+//ArrayIndexOutOfBoundsException
+String name1="ekta";
+for(int i=0; i<=name1.length();i++)
+	
+{
+	char k= name1.charAt(i);
+	System.out.println(k);
+}
+	
+//StringIndexOutOfBoundsException	
+	
+
+
+
+
+
+
+
+
+
+	
+	
+}
+	 
+	 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
